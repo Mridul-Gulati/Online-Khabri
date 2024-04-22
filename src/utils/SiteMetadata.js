b6523a52f46ad17adf1476a55922f6bca8002e6a
mridulgulati18 <mridulgulati18@gmail.com>
2024-04-22 18:44:25 +0530

    Minor

--- src/utils/SiteMetadata.js
+++ src/utils/SiteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
     description: 'premier destination for the latest in national and international news, sports, entertainment, weather, science and technology, and finance. Our blog ensures comprehensive coverage with timely updates and in-depth analysis to keep you informed on all fronts. Designed for easy navigation, OnlineKhabri provides a user-friendly experience with a commitment to journalistic integrity. Engage with our content and join discussions that matter. Whether its breaking news or detailed forecasts, stay ahead with OnlineKhabri—where news meets insight.',
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://online-khabri.vercel.app', // your website URL
+    siteUrl: 'https://onlinekhabri.com/', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social_banner.png', // add social banner in the public folder
     email: 'onlinekhabri23@gmail.com',
