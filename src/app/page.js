663ef8d71e8605e3d0746e01157af186585b5b00
mridulgulati18 <mridulgulati18@gmail.com>
2024-07-04 23:03:40 +0530

    blogs-2

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,9 @@ export default function Home() {
   console.log(allBlogs)
   return (
     <main className="flex flex-col items-center justify-center">
+      <head>
+        <meta name="google-site-verification" content="tSN9QlQTTpSHBX0GFUyydWMoTfohHzDQfx_XrBjl2jw" />
+      </head>
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPosts blogs={allBlogs} />
       <RecentPosts blogs={allBlogs} />
