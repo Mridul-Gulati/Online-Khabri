d3df7cc762a52ec6394a42990228d547a3b58009
mridulgulati18 <mridulgulati18@gmail.com>
2024-07-09 15:11:36 +0530

    push-notif

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import HomeCoverSection from '../components/Home/HomeCoverSection'
 import FeaturedPosts from '../components/Home/FeaturedPosts'
 import RecentPosts from '../components/Home/RecentPosts'
 import OneSignal from 'react-onesignal';
-
+import { useEffect } from 'react';
 export default function Home() {
   useEffect(() => {
     OneSignal.init({ appId: '660d5964-dcf7-44b7-8345-13544b9f2474' })
