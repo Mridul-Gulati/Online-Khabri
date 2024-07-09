2b0ba1eecba0ad4d542154b8617eae45220dc022
mridulgulati18 <mridulgulati18@gmail.com>
2024-07-09 16:00:21 +0530

    blog

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import { allBlogs } from '../../.contentlayer/generated'
 import HomeCoverSection from '../components/Home/HomeCoverSection'
 import FeaturedPosts from '../components/Home/FeaturedPosts'
 import RecentPosts from '../components/Home/RecentPosts'
-import OneSignal from 'react-onesignal';
 
 export default function Home() {
 
