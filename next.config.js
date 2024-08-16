85342d969c5ba43b5cdcb71e41dbe43ed79f9fea
mridulgulati18 <mridulgulati18@gmail.com>
2024-08-16 21:55:25 +0530

    Sitemap update

--- next.config.js
+++ next.config.js
@@ -5,7 +5,15 @@ const { withContentlayer } = require('next-contentlayer')
 const nextConfig = {
     compiler: {
         removeConsole: false,
-    }
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/sitemap.xml',
+                destination: '/api/sitemap',
+            },
+        ]
+    },
 }
 
 module.exports = withContentlayer({ ...nextConfig })
---
+++ src/app/api/sitemap.js
@@ -0,0 +1,65 @@
+import { allBlogs } from "@/.contentlayer/generated";
+
+const EXTERNAL_DATA_URL = 'https://www.onlinekhabri.com';
+
+function generateSiteMap(blogPosts) {
+    return `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <!-- Static URLs -->
+        <url>
+            <loc>${EXTERNAL_DATA_URL}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>yearly</changefreq>
+            <priority>1.0</priority>
+        </url>
+        <url>
+            <loc>${EXTERNAL_DATA_URL}/about</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>0.8</priority>
+        </url>
+        <url>
+            <loc>${EXTERNAL_DATA_URL}/blog</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.5</priority>
+        </url>
+        <!-- Dynamic blog URLs -->
+        ${blogPosts
+            .map(({ slug, date }) => {
+                return `
+            <url>
+                <loc>${`${EXTERNAL_DATA_URL}/blog/${slug}`}</loc>
+                <lastmod>${new Date(date).toISOString()}</lastmod>
+                <changefreq>weekly</changefreq>
+                <priority>0.5</priority>
+            </url>
+            `;
+            })
+            .join('')}
+    </urlset>
+    `;
+}
+
+export async function getServerSideProps() {
+    const blogPosts = allBlogs.map((blog) => ({
+        slug: slug(blog.title),
+        date: blog.date,
+    }));
+
+    return {
+        props: {
+            blogPosts,
+        },
+    };
+}
+
+export default function Sitemap({ blogPosts }) {
+    const sitemap = generateSiteMap(blogPosts);
+
+    return new Response(sitemap, {
+        headers: {
+            'Content-Type': 'application/xml',
+        },
+    });
+}
